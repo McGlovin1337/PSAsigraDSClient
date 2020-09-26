eaab43a1c7bc9feea315ce47ff4831603bb33437
James Riach <james@jriach.co.uk>
2020-09-26 23:44:25 +0100

    2020-09-26 - StartDSClientBackup StartDSClientBackup: Derived from BaseDSClientStartBackupSetActivity, thus inheriting Name parameter with Wildcard support. Changed Output type to DSClientStartBackupSetActivity class

--- PSAsigraDSClient/StartDSClientBackup.cs
+++ PSAsigraDSClient/StartDSClientBackup.cs
@@ -1,38 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Management.Automation;
 using AsigraDSClientApi;
 using static PSAsigraDSClient.DSClientCommon;
-using static PSAsigraDSClient.BaseDSClientRunningActivity;
 
 namespace PSAsigraDSClient
 {
     [Cmdlet(VerbsLifecycle.Start, "DSClientBackup")]
-    [OutputType(typeof(DSClientRunningActivity))]
+    [OutputType(typeof(DSClientStartBackupSetActivity))]
 
-    public class StartDSClientBackup: DSClientCmdlet
+    public class StartDSClientBackup: BaseDSClientStartBackupSetActivity
     {
-        [Parameter(Position = 0, Mandatory = true, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true, HelpMessage = "Specify the Backup Set Id")]
-        public int BackupSetId { get; set; }
-
-        [Parameter(Position = 1, HelpMessage = "Specify to Run Retention After Backup")]
+        [Parameter(HelpMessage = "Specify to Run Retention After Backup")]
         public SwitchParameter PerformRetention { get; set; }
 
-        [Parameter(Position = 2, HelpMessage = "Specify to Prescan Backup Source")]
+        [Parameter(HelpMessage = "Specify to Prescan Backup Source")]
         public SwitchParameter PreScan { get; set; }
 
-        [Parameter(Position = 3, ValueFromPipelineByPropertyName = true, HelpMessage = "Stop Backup Error Limit")]
+        [Parameter(ValueFromPipelineByPropertyName = true, HelpMessage = "Stop Backup Error Limit")]
         public int ErrorLimit { get; set; } = 0;
 
-        [Parameter(Position = 4, HelpMessage = "Specify to use Detailed Log")]
+        [Parameter(HelpMessage = "Specify to use Detailed Log")]
         public SwitchParameter UseDetailedLog { get; set; }
 
-        [Parameter(Position = 5, ValueFromPipelineByPropertyName = true, HelpMessage = "Specify Start Time")]
+        [Parameter(ValueFromPipelineByPropertyName = true, HelpMessage = "Specify Start Time")]
         public DateTime StartTime { get; set; }
 
-        protected override void DSClientProcessRecord()
+        protected override void ProcessBackupSet(BackupSet backupSet)
         {
-            BackupSet backupSet = DSClientSession.backup_set(BackupSetId);
-
             BackupSetItem[] backupSetItems = backupSet.items();
 
             BackupStartParameters startParams = backupSet.createDefaultBackupParameters();
@@ -54,17 +49,67 @@ namespace PSAsigraDSClient
             WriteVerbose("Submitting Backup Start Activity...");
             GenericActivity backupActivity = backupSet.start_backup(backupSetItems, startParams);
 
-            int backupActivityId = backupActivity.getID();
-            WriteVerbose("Backup Activity with ID " + backupActivityId + " submitted");
+            DSClientStartBackupSetActivity startActivity = new DSClientStartBackupSetActivity
+            {
+                ActivityId = backupActivity.getID(),
+                BackupSetId = backupSet.getID(),
+                Name = backupSet.getName()
+            };
 
-            running_activity_info backupActivityInfo = backupActivity.getCurrentStatus();
-            DSClientRunningActivity runningBackup = new DSClientRunningActivity(backupActivityInfo);
-
-            WriteObject(runningBackup);
+            WriteObject(startActivity);
 
             backupActivity.Dispose();
             startParams.Dispose();
-            backupSet.Dispose();
+        }
+
+        protected override void ProcessBackupSets(BackupSet[] backupSets)
+        {
+            List<DSClientStartBackupSetActivity> startActivity = new List<DSClientStartBackupSetActivity>();
+
+            foreach (BackupSet backupSet in backupSets)
+            {
+                try
+                {
+                    BackupSetItem[] backupSetItems = backupSet.items();
+
+                    BackupStartParameters startParams = backupSet.createDefaultBackupParameters();
+
+                    if (MyInvocation.BoundParameters.ContainsKey("PerformRetention"))
+                        startParams.setDoRetentionAfter(PerformRetention);
+
+                    if (MyInvocation.BoundParameters.ContainsKey("PreScan"))
+                        startParams.setPreScanSource(PreScan);
+
+                    startParams.setErrorStop(ErrorLimit);
+
+                    if (MyInvocation.BoundParameters.ContainsKey("UseDetailedLog"))
+                        startParams.setUsesDetailedLog(UseDetailedLog);
+
+                    if (MyInvocation.BoundParameters.ContainsKey("StartTime"))
+                        startParams.setStartTime(DateTimeToUnixEpoch(StartTime));
+
+                    WriteVerbose("Submitting Backup Start Activity...");
+                    GenericActivity backupActivity = backupSet.start_backup(backupSetItems, startParams);
+
+                    startActivity.Add(new DSClientStartBackupSetActivity
+                    {
+                        ActivityId = backupActivity.getID(),
+                        BackupSetId = backupSet.getID(),
+                        Name = backupSet.getName()
+                    });
+
+                    backupActivity.Dispose();
+                    startParams.Dispose();
+                }
+                catch (APIException e)
+                {
+                    WriteWarning(e.Message);
+
+                    continue;
+                }
+            }
+
+            startActivity.ForEach(WriteObject);
         }
     }
 }

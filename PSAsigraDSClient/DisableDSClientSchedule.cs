91da43a474a05fcb4323d6a954b37596be891305
James Riach <james@jriach.co.uk>
2020-10-14 23:38:39 +0100

    2020-10-14 - Schedules Created Enable-DSClientSchedule Cmdlet Created Disable-DSClientSchedule Cmdlet

---
+++ PSAsigraDSClient/DisableDSClientSchedule.cs
@@ -0,0 +1,28 @@
+﻿using System.Management.Automation;
+using AsigraDSClientApi;
+
+namespace PSAsigraDSClient
+{
+    [Cmdlet(VerbsLifecycle.Disable, "DSClientSchedule")]
+
+    public class DisableDSClientSchedule: DSClientCmdlet
+    {
+        [Parameter(Position = 0, Mandatory = true, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true, HelpMessage = "Specify the Schedule to Disable")]
+        public int ScheduleId { get; set; }
+
+        protected override void DSClientProcessRecord()
+        {
+            // Get the Schedule from DS-Client
+            WriteVerbose("Retrieving Schedule from DS-Client...");
+            ScheduleManager DSClientScheduleMgr = DSClientSession.getScheduleManager();
+            Schedule schedule = DSClientScheduleMgr.definedSchedule(ScheduleId);
+
+            // Set the Schedule to InActive
+            WriteVerbose("Performing action: Disable On Target: " + ScheduleId);
+            schedule.setActive(false);
+
+            schedule.Dispose();
+            DSClientScheduleMgr.Dispose();
+        }
+    }
+}
---
+++ PSAsigraDSClient/EnableDSClientSchedule.cs
@@ -0,0 +1,28 @@
+﻿using System.Management.Automation;
+using AsigraDSClientApi;
+
+namespace PSAsigraDSClient
+{
+    [Cmdlet(VerbsLifecycle.Enable, "DSClientSchedule")]
+
+    public class EnableDSClientSchedule: DSClientCmdlet
+    {
+        [Parameter(Position = 0, Mandatory = true, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true, HelpMessage = "Specify the Schedule to Enable")]
+        public int ScheduleId { get; set; }
+
+        protected override void DSClientProcessRecord()
+        {
+            // Get the Schedule from DS-Client
+            WriteVerbose("Retrieving Schedule from DS-Client...");
+            ScheduleManager DSClientScheduleMgr = DSClientSession.getScheduleManager();
+            Schedule schedule = DSClientScheduleMgr.definedSchedule(ScheduleId);
+
+            // Set the Schedule to Active
+            WriteVerbose("Performing action: Enable On Target: " + ScheduleId);
+            schedule.setActive(true);
+
+            schedule.Dispose();
+            DSClientScheduleMgr.Dispose();
+        }
+    }
+}

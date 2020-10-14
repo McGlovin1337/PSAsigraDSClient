effe5d2ea0cdb8bf9d0748f237bf0afb7fc419a3
James Riach <james@jriach.co.uk>
2020-10-14 22:38:11 +0100

    2020-10-14  - Backup Set Modification Created Enable-DSClientBackupSet Cmdlet Created Disable-DSClientBackupSet Cmdlet

---
+++ PSAsigraDSClient/DisableDSClientBackupSet.cs
@@ -0,0 +1,26 @@
+﻿using System.Management.Automation;
+using AsigraDSClientApi;
+
+namespace PSAsigraDSClient
+{
+    [Cmdlet(VerbsLifecycle.Disable, "DSClientBackupSet")]
+
+    public class DisableDSClientBackupSet: DSClientCmdlet
+    {
+        [Parameter(Position = 0, Mandatory = true, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true, HelpMessage = "Specify the Backup Set to Disable")]
+        public int BackupSetId { get; set; }
+
+        protected override void DSClientProcessRecord()
+        {
+            // Get the Backup Set from DS-Client
+            WriteVerbose("Retrieving Backup Set from DS-Client...");
+            BackupSet backupSet = DSClientSession.backup_set(BackupSetId);
+
+            // Set the Backup Set to InActive
+            WriteVerbose("Performing action: Disable On Target: " + BackupSetId);
+            backupSet.setActive(false);
+
+            backupSet.Dispose();
+        }
+    }
+}
---
+++ PSAsigraDSClient/EnableDSClientBackupSet.cs
@@ -0,0 +1,26 @@
+﻿using System.Management.Automation;
+using AsigraDSClientApi;
+
+namespace PSAsigraDSClient
+{
+    [Cmdlet(VerbsLifecycle.Enable, "DSClientBackupSet")]
+
+    public class EnableDSClientBackupSet: DSClientCmdlet
+    {
+        [Parameter(Position = 0, Mandatory = true, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true, HelpMessage = "Specify the Backup Set to Enable")]
+        public int BackupSetId { get; set; }
+
+        protected override void DSClientProcessRecord()
+        {
+            // Get the Backup Set from DS-Client
+            WriteVerbose("Retrieving Backup Set from DS-Client...");
+            BackupSet backupSet = DSClientSession.backup_set(BackupSetId);
+
+            // Set the Backup Set to Active
+            WriteVerbose("Performing action: Enable On Target: " + BackupSetId);
+            backupSet.setActive(true);
+
+            backupSet.Dispose();
+        }
+    }
+}

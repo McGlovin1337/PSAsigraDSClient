1d510cf663dfa94e96b447e2abe9bd2c1ab5b81b
James Riach <james@jriach.co.uk>
2020-10-18 17:51:13 +0100

    2020-10-18 - Advanced Config Created BaseDSClientAdvancedConfig Base Class GetDSClientAdvancedConfig now derived from Base Class Created Set-DSClientAdvancedConfig Cmdlet

---
+++ PSAsigraDSClient/BaseDSClientAdvancedConfig.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using AsigraDSClientApi;
+
+namespace PSAsigraDSClient
+{
+    public abstract class BaseDSClientAdvancedConfig : DSClientCmdlet
+    {
+        protected abstract void ProcessAdavancedConfig(IEnumerable<advanced_config_info> advancedConfigInfo);
+
+        protected override void DSClientProcessRecord()
+        {
+            ClientConfiguration DSClientConfigMgr = DSClientSession.getConfigurationManager();
+
+            WriteVerbose("Retrieving DS-Client Advanced Configuration Parameters...");
+            advanced_config_info[] advancedConfigInfo = DSClientConfigMgr.getAdvancedConfigurationParameters();
+
+            DSClientConfigMgr.Dispose();
+
+            ProcessAdavancedConfig(advancedConfigInfo);
+        }
+
+        protected class DSClientAdvancedConfig
+        {
+            public string Category { get; set; }
+            public string Name { get; set; }
+            public string Value { get; set; }
+            public string DefaultValue { get; set; }
+            public bool RestartRequired { get; set; }
+            public bool ReconnectRequired { get; set; }
+            public bool HasDefaultValue { get; set; }
+            public bool UseLocalSetting { get; set; }
+            public bool LocalItem { get; set; }
+            public bool GlobalItem { get; set; }
+            public int NodeId { get; set; }
+
+            public DSClientAdvancedConfig(advanced_config_info advancedConfig)
+            {
+                Category = EAdvConfigCategoryToString(advancedConfig.category);
+                Name = advancedConfig.name;
+                Value = advancedConfig.value;
+                DefaultValue = advancedConfig.default_value;
+                RestartRequired = ((advancedConfig.flags & (int)EDSClientConfigurationFlags.EDSClientConfigurationFlags__NeedRestartDSClient) > 0) ? true : false;
+                ReconnectRequired = ((advancedConfig.flags & (int)EDSClientConfigurationFlags.EDSClientConfigurationFlags__NeedReconnectDSClient) > 0) ? true : false;
+                HasDefaultValue = ((advancedConfig.flags & (int)EDSClientConfigurationFlags.EDSClientConfigurationFlags__HasDefaultValue) > 0) ? true : false;
+                UseLocalSetting = ((advancedConfig.flags & (int)EDSClientConfigurationFlags.EDSClientConfigurationFlags__UseLocalSetting) > 0) ? true : false;
+                LocalItem = ((advancedConfig.flags & (int)EDSClientConfigurationFlags.EDSClientConfigurationFlags__LocalItem) > 0) ? true : false;
+                GlobalItem = ((advancedConfig.flags & (int)EDSClientConfigurationFlags.EDSClientConfigurationFlags__GlobalItem) > 0) ? true : false;
+                NodeId = advancedConfig.ds_client_id;
+            }
+
+            private string EAdvConfigCategoryToString(EAdvConfigCategory category)
+            {
+                string Category = null;
+
+                switch (category)
+                {
+                    case EAdvConfigCategory.EAdvConfigCategory__APIConectivity:
+                        Category = "ApiConnectivity";
+                        break;
+                    case EAdvConfigCategory.EAdvConfigCategory__Admin:
+                        Category = "Admin";
+                        break;
+                    case EAdvConfigCategory.EAdvConfigCategory__Miscellaneous:
+                        Category = "Miscellaneous";
+                        break;
+                    case EAdvConfigCategory.EAdvConfigCategory__Notification:
+                        Category = "Notification";
+                        break;
+                    case EAdvConfigCategory.EAdvConfigCategory__Performance:
+                        Category = "Performance";
+                        break;
+                    case EAdvConfigCategory.EAdvConfigCategory__Communication:
+                        Category = "Communication";
+                        break;
+                    case EAdvConfigCategory.EAdvConfigCategory__Installation:
+                        Category = "Installation";
+                        break;
+                }
+
+                return Category;
+            }
+        }
+    }
+}
---
+++ PSAsigraDSClient/SetDSClientAdvancedConfig.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using AsigraDSClientApi;
+
+namespace PSAsigraDSClient
+{
+    [Cmdlet(VerbsCommon.Set, "DSClientAdvancedConfig")]
+
+    public class SetDSClientAdvancedConfig: BaseDSClientAdvancedConfig
+    {
+        [Parameter(Position = 0, Mandatory = true, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true, HelpMessage = "Specify the Advanced Config Item to modify")]
+        [ValidateNotNullOrEmpty]
+        public string Name { get; set; }
+
+        [Parameter(Position = 1, Mandatory = true, ValueFromPipelineByPropertyName = true, HelpMessage = "Specify the Advanced Config Item Value")]
+        [ValidateNotNullOrEmpty]
+        public string Value { get; set; }
+
+        protected override void ProcessAdavancedConfig(IEnumerable<advanced_config_info> advancedConfigInfo)
+        {
+            // Get the config item (validates the config item exists, will throw an exception if it doesn't)
+            advanced_config_info configItem = advancedConfigInfo.Single(item => item.name == Name);
+
+            ClientConfiguration DSClientConfigMgr = DSClientSession.getConfigurationManager();
+
+            // Set the config value
+            if (configItem.type == EAdvConfigType.EAdvConfigType__ConfigTypeString)
+                DSClientConfigMgr.setAdvancedConfigString(Name, Value);
+            else if (configItem.type == EAdvConfigType.EAdvConfigType__ConfigTypeNumber)
+                DSClientConfigMgr.setAdvancedConfigNumber(Name, Convert.ToInt32(Value));
+
+            DSClientConfigMgr.Dispose();
+        }
+    }
+}

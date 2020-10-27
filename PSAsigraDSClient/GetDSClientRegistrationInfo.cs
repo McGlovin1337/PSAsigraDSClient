38035e782232edab921559313e8dc1f94c51a30e
James Riach <james@jriach.co.uk>
2020-10-27 22:41:33 +0000

    2020-10-27 - DS-Client Registration Extended DSClientRegistrationInfo class to include 'End User' details Get-DSClientRegistrationInfo Cmdlet now returns 'End User' details Set-DSClientRegistrationInfo Cmdlet now supports setting 'End User' details Removed Mandatory Parameters from Set-DSClientRegistrationInfo Cmdlet Set-DSClientRegistrationInfo Cmdlet now allows updating of individual Properties rather than creating a new dsc_reg_info object each time

--- PSAsigraDSClient/GetDSClientRegistrationInfo.cs
+++ PSAsigraDSClient/GetDSClientRegistrationInfo.cs
@@ -11,10 +11,17 @@ namespace PSAsigraDSClient
         {
             ClientConfiguration DSClientConfig = DSClientSession.getConfigurationManager();
 
-            DSClientRegistrationInfo DSCregInfo = new DSClientRegistrationInfo(DSClientConfig.getDSCRegistrationInfo());
+            dsc_reg_info dscRegInfo = DSClientConfig.getDSCRegistrationInfo();
+
+            UserInfoConfiguration DSClientUserInfoConfig = DSClientConfig.getUserInfoConfiguration();
+
+            user_info userInfo = DSClientUserInfoConfig.getUserInfo();
+
+            DSClientRegistrationInfo DSCregInfo = new DSClientRegistrationInfo(dscRegInfo, userInfo);
 
             WriteObject(DSCregInfo);
 
+            DSClientUserInfoConfig.Dispose();
             DSClientConfig.Dispose();
         }
 
@@ -29,8 +36,11 @@ namespace PSAsigraDSClient
             public string PrivateKey { get; set; }
             public string PrivateKeyEncryption { get; set; }
             public bool EscrowKeys { get; set; }
+            public int CountryCode { get; set; }
+            public string Industry { get; set; }
+            public string Employees { get; set; }
 
-            public DSClientRegistrationInfo(dsc_reg_info regInfo)
+            public DSClientRegistrationInfo(dsc_reg_info regInfo, user_info userInfo)
             {
                 AccountNumber = regInfo.account_num;
                 CustomerName = regInfo.customer_name;
@@ -41,6 +51,76 @@ namespace PSAsigraDSClient
                 PrivateKey = regInfo.private_key;
                 PrivateKeyEncryption = EEncryptionTypeToString(regInfo.private_key_enc);
                 EscrowKeys = regInfo.escrow_enc_key;
+                CountryCode = userInfo.country_code;
+                Industry = EIndustryVerticalToString(userInfo.industry_code);
+                Employees = ENumberOfEmployeesToString(userInfo.num_of_employees);
+            }
+
+            private static string ENumberOfEmployeesToString(ENumberOfEmployees employees)
+            {
+                string Employees = null;
+
+                switch(employees)
+                {
+                    case ENumberOfEmployees.ENumberOfEmployees__Size1:
+                        Employees = "1-50";
+                        break;
+                    case ENumberOfEmployees.ENumberOfEmployees__Size2:
+                        Employees = "51-200";
+                        break;
+                    case ENumberOfEmployees.ENumberOfEmployees__Size3:
+                        Employees = "201-500";
+                        break;
+                    case ENumberOfEmployees.ENumberOfEmployees__Size4:
+                        Employees = "501-1000";
+                        break;
+                    case ENumberOfEmployees.ENumberOfEmployees__Size5:
+                        Employees = "1001-5000";
+                        break;
+                    case ENumberOfEmployees.ENumberOfEmployees__Size6:
+                        Employees = "5001-10000";
+                        break;
+                    case ENumberOfEmployees.ENumberOfEmployees__Size7:
+                        Employees = "10001+";
+                        break;
+                }
+
+                return Employees;
+            }
+
+            private static string EIndustryVerticalToString(EIndustryVertical industry)
+            {
+                string Industry = null;
+
+                switch(industry)
+                {
+                    case EIndustryVertical.EIndustryVertical__ManufacturingAndEnergy:
+                        Industry = "ManufacturingAndEnergy";
+                        break;
+                    case EIndustryVertical.EIndustryVertical__RetailAndWholesaleTrade:
+                        Industry = "RetailAndWholesaleTrade";
+                        break;
+                    case EIndustryVertical.EIndustryVertical__UtilitiesAndTelecommunications:
+                        Industry = "UtilitiesAndTelecommunications";
+                        break;
+                    case EIndustryVertical.EIndustryVertical__ComputersHardwareSoftware:
+                        Industry = "ComputersHardwareSoftware";
+                        break;
+                    case EIndustryVertical.EIndustryVertical__BusinessServicesAndConstruction:
+                        Industry = "BusinessServicesAndConstruction";
+                        break;
+                    case EIndustryVertical.EIndustryVertical__MediaEntertainmentAndLeisure:
+                        Industry = "MediaEntertainmentAndLeisure";
+                        break;
+                    case EIndustryVertical.EIndustryVertical__FinancialServicesAndInsurance:
+                        Industry = "FinancialServicesAndInsurance";
+                        break;
+                    case EIndustryVertical.EIndustryVertical__PublicSectorAndHealthcare:
+                        Industry = "PublicSectorAndHealthcare";
+                        break;
+                }
+
+                return Industry;
             }
 
             private static string EEncryptionTypeToString(EEncryptionType encryptionType)
--- PSAsigraDSClient/SetDSClientRegistrationInfo.cs
+++ PSAsigraDSClient/SetDSClientRegistrationInfo.cs
@@ -11,19 +11,19 @@ namespace PSAsigraDSClient
         [ValidateNotNullOrEmpty]
         public string DsSystemAddress { get; set; }
 
-        [Parameter(Position = 1, Mandatory = true, ValueFromPipelineByPropertyName = true, HelpMessage = "Specify Account Number")]
+        [Parameter(Position = 1, ValueFromPipelineByPropertyName = true, HelpMessage = "Specify Account Number")]
         [ValidateNotNullOrEmpty]
         public string AccountNumber { get; set; }
 
-        [Parameter(Position = 2, Mandatory = true, ValueFromPipelineByPropertyName = true, HelpMessage = "Specify Customer Name")]
+        [Parameter(Position = 2, ValueFromPipelineByPropertyName = true, HelpMessage = "Specify Customer Name")]
         [ValidateNotNullOrEmpty]
         public string CustomerName { get; set; }
 
-        [Parameter(Position = 3, Mandatory = true, ValueFromPipelineByPropertyName = true, HelpMessage = "Specify Client Number")]
+        [Parameter(Position = 3, ValueFromPipelineByPropertyName = true, HelpMessage = "Specify Client Number")]
         [ValidateNotNullOrEmpty]
         public string ClientNumber { get; set; }
 
-        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true, HelpMessage = "Specify the Account Key")]
+        [Parameter(ValueFromPipelineByPropertyName = true, HelpMessage = "Specify the Account Key")]
         [ValidateNotNullOrEmpty]
         public string AccountKey { get; set; }
 
@@ -31,7 +31,7 @@ namespace PSAsigraDSClient
         [ValidateSet("DES", "AES128", "AES192", "AES256", "AES128IV", "AES192IV", "AES256IV")]
         public string AccountEncryption { get; set; } = "AES128";
 
-        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true, HelpMessage = "Specify Private Key")]
+        [Parameter(ValueFromPipelineByPropertyName = true, HelpMessage = "Specify Private Key")]
         [ValidateNotNullOrEmpty]
         public string PrivateKey { get; set; }
 
@@ -42,27 +42,129 @@ namespace PSAsigraDSClient
         [Parameter(HelpMessage = "Specify if to Forward Encryption Keys to DS-System")]
         public SwitchParameter EscrowKeys { get; set; }
 
+        [Parameter(HelpMessage = "Set the ISO 3166 Country Code")]
+        public int CountryCode { get; set; }
+
+        [Parameter(HelpMessage = "Specify the Industry Vertical of the End User Organisation")]
+        [ValidateSet("ManufacturingAndEnergy", "RetailAndWholesaleTrade", "UtilitiesAndTelecommunications", "ComputersHardwareSoftware", "BusinessServicesAndConstruction", "MediaEntertainmentAndLeisure", "FinancialServicesAndInsurance", "PublicSectorAndHealthcare")]
+        public string Industry { get; set; }
+
+        [Parameter(HelpMessage = "Specify the Number of Employees of the End User Organisation")]
+        public int Employees { get; set; }
+
         protected override void DSClientProcessRecord()
         {
-            dsc_reg_info RegInfo = new dsc_reg_info
-            {
-                account_key = AccountKey,
-                account_key_enc = StringToEEncryptionType(AccountEncryption),
-                account_num = AccountNumber,
-                client_num = ClientNumber,
-                customer_name = CustomerName,
-                dssys_ip_addr = DsSystemAddress,
-                escrow_enc_key = EscrowKeys,
-                private_key = PrivateKey,
-                private_key_enc = StringToEEncryptionType(PrivateKeyEncryption)
-            };
-
-            WriteVerbose("Setting DS-Client Registration...");
+            // Retrieve the current Registration Info
             ClientConfiguration DSClientConfgMgr = DSClientSession.getConfigurationManager();
 
-            DSClientConfgMgr.setDSCRegistrationInfo(RegInfo);
+            dsc_reg_info regInfo = DSClientConfgMgr.getDSCRegistrationInfo();
 
+            // Set the Registration Info
+            if (AccountKey != null)
+                regInfo.account_key = AccountKey;
+
+            if (AccountEncryption != null)
+                regInfo.account_key_enc = StringToEEncryptionType(AccountEncryption);
+
+            if (AccountNumber != null)
+                regInfo.account_num = AccountNumber;
+
+            if (ClientNumber != null)
+                regInfo.client_num = ClientNumber;
+
+            if (CustomerName != null)
+                regInfo.customer_name = CustomerName;
+
+            if (DsSystemAddress != null)
+                regInfo.dssys_ip_addr = DsSystemAddress;
+
+            if (MyInvocation.BoundParameters.ContainsKey("EscrowKeys"))
+                regInfo.escrow_enc_key = EscrowKeys;
+
+            if (PrivateKey != null)
+                regInfo.private_key = PrivateKey;
+
+            if (PrivateKeyEncryption != null)
+                regInfo.private_key_enc = StringToEEncryptionType(PrivateKeyEncryption);
+
+            // Retrieve the current 'End User' Info
+            UserInfoConfiguration DSClientUserInfoConfig = DSClientConfgMgr.getUserInfoConfiguration();
+
+            user_info userInfo = DSClientUserInfoConfig.getUserInfo();
+
+            // Set the 'End User' Info
+            if (MyInvocation.BoundParameters.ContainsKey("CountryCode"))
+                userInfo.country_code = CountryCode;
+
+            if (Industry != null)
+                userInfo.industry_code = StringToEIndustryVertical(Industry);
+
+            if (MyInvocation.BoundParameters.ContainsKey("Employees"))
+                userInfo.num_of_employees = IntToENumberOfEmployees(Employees);
+
+            WriteVerbose("Setting DS-Client Registration...");
+            DSClientConfgMgr.setDSCRegistrationInfo(regInfo);
+            DSClientUserInfoConfig.setUserInfo(userInfo);
+
+            DSClientUserInfoConfig.Dispose();
             DSClientConfgMgr.Dispose();
+        }
+
+        private static ENumberOfEmployees IntToENumberOfEmployees(int employees)
+        {
+            ENumberOfEmployees Employees = ENumberOfEmployees.ENumberOfEmployees__UNDEFINED;
+
+            if (employees < 51)
+                Employees = ENumberOfEmployees.ENumberOfEmployees__Size1;
+            else if (employees > 50 && employees < 201)
+                Employees = ENumberOfEmployees.ENumberOfEmployees__Size2;
+            else if (employees > 200 && employees < 501)
+                Employees = ENumberOfEmployees.ENumberOfEmployees__Size3;
+            else if (employees > 500 && employees < 1001)
+                Employees = ENumberOfEmployees.ENumberOfEmployees__Size4;
+            else if (employees > 1000 && employees < 5001)
+                Employees = ENumberOfEmployees.ENumberOfEmployees__Size5;
+            else if (employees > 5000 && employees < 10001)
+                Employees = ENumberOfEmployees.ENumberOfEmployees__Size6;
+            else if (employees > 10000)
+                Employees = ENumberOfEmployees.ENumberOfEmployees__Size7;
+
+            return Employees;
+        }
+
+        private static EIndustryVertical StringToEIndustryVertical(string industry)
+        {
+            EIndustryVertical Industry = EIndustryVertical.EIndustryVertical__UNDEFINED;
+
+            switch(industry)
+            {
+                case "ManufacturingAndEnergy":
+                    Industry = EIndustryVertical.EIndustryVertical__ManufacturingAndEnergy;
+                    break;
+                case "RetailAndWholesaleTrade":
+                    Industry = EIndustryVertical.EIndustryVertical__RetailAndWholesaleTrade;
+                    break;
+                case "UtilitiesAndTelecommunications":
+                    Industry = EIndustryVertical.EIndustryVertical__UtilitiesAndTelecommunications;
+                    break;
+                case "ComputersHardwareSoftware":
+                    Industry = EIndustryVertical.EIndustryVertical__ComputersHardwareSoftware;
+                    break;
+                case "BusinessServicesAndConstruction":
+                    Industry = EIndustryVertical.EIndustryVertical__BusinessServicesAndConstruction;
+                    break;
+                case "MediaEntertainmentAndLeisure":
+                    Industry = EIndustryVertical.EIndustryVertical__MediaEntertainmentAndLeisure;
+                    break;
+                case "FinancialServicesAndInsurance":
+                    Industry = EIndustryVertical.EIndustryVertical__FinancialServicesAndInsurance;
+                    break;
+                case "PublicSectorAndHealthcare":
+                    Industry = EIndustryVertical.EIndustryVertical__PublicSectorAndHealthcare;
+                    break;
+            }
+
+            return Industry;
         }
 
         private static EEncryptionType StringToEEncryptionType(string encryptionType)

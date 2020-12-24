4fd0fe9ef37f1a0e72845e659b1b540203dfa3cb
James Riach <james@jriach.co.uk>
2020-12-24 11:24:15 +0000

    Added "ValueFromPipelineByPropertyName" to ScheduleId parameter in Remove-DSClientSchedule Cmdlet to improve pipelining between Cmdlets

--- PSAsigraDSClient/RemoveDSClientSchedule.cs
+++ PSAsigraDSClient/RemoveDSClientSchedule.cs
@@ -7,7 +7,7 @@ namespace PSAsigraDSClient
 
     public class RemoveDSClientSchedule: DSClientCmdlet
     {
-        [Parameter(Position = 0, Mandatory = true, ValueFromPipeline = true, HelpMessage = "The ScheduleId to remove")]
+        [Parameter(Position = 0, Mandatory = true, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true, HelpMessage = "The ScheduleId to remove")]
         [ValidateNotNullOrEmpty]
         public int ScheduleId { get; set; }
 

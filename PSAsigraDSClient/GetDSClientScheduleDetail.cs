1e1920a0698c03932ab99b0aa6c5b584916e080a
James Riach <james@jriach.co.uk>
2021-04-17 23:44:46 +0100

    2021-04-17 - Schedule Detail Progress Indicators
    Improved the Progress Indicator Output in Get-DSClientScheduleDetail Cmdlet

--- PSAsigraDSClient/GetDSClientScheduleDetail.cs
+++ PSAsigraDSClient/GetDSClientScheduleDetail.cs
@@ -44,45 +44,61 @@ namespace PSAsigraDSClient
             Dictionary<string, ScheduleDetail> unidentified = new Dictionary<string, ScheduleDetail>();
 
             int detailCounter = 0;
+            int hashCounter = 0;
             WriteVerbose("Performing Action: Process Schedule Details");
             ProgressRecord progressRecord = new ProgressRecord(1, "Process Schedule Details", $"0 of {detailCount} processed, 0%")
             {
-                RecordType = ProgressRecordType.Processing
+                RecordType = ProgressRecordType.Processing,
+                CurrentOperation = "Processing Schedule Detail"
             };
             foreach (ScheduleDetail detail in ScheduleDetails)
             {
-                progressRecord.CurrentOperation = "Processing Schedule Detail";
-                progressRecord.PercentComplete = (int)Math.Round((double)(((double)detailCounter) / (double)detailCount) * 100);
+                progressRecord.PercentComplete = (int)Math.Round((double)(((double)detailCounter + (double)hashCounter) / ((double)detailCount * 2)) * 100);
                 progressRecord.StatusDescription = $"{detailCounter} of {detailCount} processed, {progressRecord.PercentComplete}%";
                 WriteProgress(progressRecord);
-                string detailHash = ScheduleDetailHash(Schedule, detail);
-                WriteDebug($"Computed Hash: {detailHash}");
 
+                string detailHash = ScheduleDetailHash(Schedule, detail);
+                hashCounter++;
+                WriteDebug($"Computed Hash: {detailHash}");
+                progressRecord.PercentComplete = (int)Math.Round((double)(((double)detailCounter + (double)hashCounter) / ((double)detailCount * 2)) * 100);
+                progressRecord.StatusDescription = $"{detailCounter} of {detailCount} processed, {progressRecord.PercentComplete}%";
+                WriteProgress(progressRecord);
+
+                // Check if the Hash is already in the Dictionary and if so fetch the associated Id
                 detailHashes.TryGetValue(detailHash, out int id);
                 if (id > 0)
                 {
+                    WriteDebug("Hash found in SessionState");
                     if (id >= startingId)
                         startingId = id + 1;
                     DSClientScheduleDetail.Add(new DSClientScheduleDetail(id, scheduleInfo, detail));
                     detail.Dispose();
+                    detailCounter++;
                 }
                 else
+                {
+                    WriteDebug("Hash not found in SessionState");
                     unidentified.Add(detailHash, detail);
+                }
+            }
 
+            // Process all the Schedule Details that were not already in the Dictionary, and add them
+            foreach (KeyValuePair<string, ScheduleDetail> hash in unidentified)
+            {
+                progressRecord.PercentComplete = (int)Math.Round((double)(((double)detailCounter + hashCounter) / ((double)detailCount * 2)) * 100);
+                progressRecord.StatusDescription = $"{detailCounter} of {detailCount} processed, {progressRecord.PercentComplete}%";
+                WriteProgress(progressRecord);
+
+                DSClientScheduleDetail.Add(new DSClientScheduleDetail(startingId, scheduleInfo, hash.Value));
+                hash.Value.Dispose();
+                detailHashes.Add(hash.Key, startingId);
+                startingId++;
                 detailCounter++;
             }
 
             progressRecord.RecordType = ProgressRecordType.Completed;
             progressRecord.PercentComplete = (int)Math.Round((double)(((double)detailCounter) / (double)detailCount) * 100);
             WriteProgress(progressRecord);
-
-            foreach (KeyValuePair<string, ScheduleDetail> hash in unidentified)
-            {
-                DSClientScheduleDetail.Add(new DSClientScheduleDetail(startingId, scheduleInfo, hash.Value));
-                hash.Value.Dispose();
-                detailHashes.Add(hash.Key, startingId);
-                startingId++;
-            }
 
             SessionState.PSVariable.Set("ScheduleDetail", detailHashes);
 

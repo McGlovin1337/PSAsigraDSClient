d9dbc9ba11ecd8fda67e03758900431048b1c838
James Riach <james@jriach.co.uk>
2021-06-16 22:23:29 +0100

    Add Cmdlet to Remove Items from Restore Session
    Update Module Manifest with new Cmdlets

---
+++ PSAsigraDSClient/RemoveDSClientRestoreItem.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+
+namespace PSAsigraDSClient
+{
+    [Cmdlet(VerbsCommon.Remove, "DSClientRestoreItem", SupportsShouldProcess = true)]
+    [OutputType(typeof(void))]
+
+    public class RemoveDSClientRestoreItem : DSClientCmdlet
+    {
+        [Parameter(Position = 0, Mandatory = true, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true, HelpMessage = "Specify the Restore Session to remove item from")]
+        public int RestoreId { get; set; }
+
+        [Parameter(ValueFromPipelineByPropertyName = true, HelpMessage = "Specify Item(s) to remove by Id")]
+        public long[] ItemId { get; set; }
+
+        protected override void DSClientProcessRecord()
+        {
+            WriteVerbose("Performing Action: Retrieve Restore Sessions");
+            List<DSClientRestoreSession> restoreSessions = SessionState.PSVariable.GetValue("RestoreSessions", null) as List<DSClientRestoreSession>;
+
+            if (restoreSessions == null)
+                throw new Exception("No Restore Sessions Found");
+
+            bool found = false;
+            for (int i = 0; i < restoreSessions.Count(); i++)
+            {
+                if (restoreSessions[i].RestoreId == RestoreId)
+                {
+                    DSClientRestoreSession restoreSession = restoreSessions[i];
+
+                    if(ShouldProcess($"Restore Session with Id '{RestoreId}'", "Remove Items for Restore"))
+                        restoreSession.RemoveSelectedItems(ItemId);
+
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found)
+                throw new Exception("Restore Session Not Found");
+        }
+    }
+}

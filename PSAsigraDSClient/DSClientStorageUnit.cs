af0a230a5eaefb02882d1172c4f4a9b20cd72fba
James Riach <james@jriach.co.uk>
2021-11-01 16:10:36 +0000

    Issue #142 - Storage Units
    Add new DSClientStorageUnit Class

---
+++ PSAsigraDSClient/DSClientStorageUnit.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSAsigraDSClient
+{
+    public class DSClientStorageUnit
+    {
+        public long Bytes { get; private set; }
+        public decimal Kilobytes { get; private set; }
+        public decimal Megabytes { get; private set; }
+        public decimal Gigabytes { get; private set; }
+        public decimal Terrabytes { get; private set; }
+        public decimal Petabytes { get; private set; }
+
+        public DSClientStorageUnit(long bytes)
+        {
+            Bytes = bytes;
+            Kilobytes = bytes / 1024;
+            Megabytes = Kilobytes / 1024;
+            Gigabytes = Megabytes / 1024;
+            Terrabytes = Gigabytes / 1024;
+            Petabytes = Terrabytes / 1024;
+        }
+
+        public override string ToString()
+        {
+            if (Petabytes >= 1)
+                return $"{Math.Round(Petabytes, 2)} PB";
+            else if (Terrabytes >= 1)
+                return $"{Math.Round(Terrabytes, 2)} TB";
+            else if (Gigabytes >= 1)
+                return $"{Math.Round(Gigabytes, 2)} GB";
+            else if (Megabytes >= 1)
+                return $"{Math.Round(Megabytes, 2)} MB";
+            else if (Kilobytes >= 1)
+                return $"{Math.Round(Kilobytes, 2)} KB";
+            else
+                return $"{Bytes} B";
+        }
+    }
+}
